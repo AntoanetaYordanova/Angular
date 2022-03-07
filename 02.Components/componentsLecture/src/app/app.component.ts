f678f4c3b9b9d4e0ef9b59973d42dc831860e190
Antoaneta Yordanova <antoaneta.yordanova@tutanota.com>
2022-03-07 17:37:51 +0100

    Angular

--- 02.Components/componentsLecture/src/app/app.component.ts
+++ 02.Components/componentsLecture/src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'componentsLecture';
+  showText = true;
+  title = '1';
+
+  users = [{
+    name : 'Toni',
+    age : 31
+  }, 
+  {
+    name : 'Ivan',
+    age : 13
+  }, 
+  {
+    name : 'Petkan',
+    age : 311
+  }, 
+]
+
+  ngAfterViewInit(): void{
+    debugger;
+  }
+
+  changeTitle(inputEl : HTMLInputElement){    
+    this.title = inputEl.value;
+    inputEl.value = '';
+  }
+
+  // buttonClickHandler(): void {
+  //   this.title++;  
+  // }
+
+  toggleText(ev : Event): void{
+    console.log(ev);
+    
+    this.showText = !this.showText;
+  }
 }
--- 02.Components/componentsLecture/src/app/app.module.ts
+++ 02.Components/componentsLecture/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserListItemComponent } from './user-list-item/user-list-item.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    UserListComponent,
+    UserListItemComponent
   ],
   imports: [
     BrowserModule
